a1d41efc0e26ff55ec297691533d67d193212a53
Micael Luan Conti <micael.conti@govbr.com.br>
2026-01-21 17:39:25 -0300

    melhorias

--- main.py
+++ main.py
@@ -13,21 +13,23 @@ database = os.getenv("DATABASE")
 username = os.getenv("USERNAME")
 password = os.getenv("PASSWORD")
 
-matriculas = [(1170597,1),(1160290,1),(1164783,1),(1153544,1),(1142356,1),(1148290,1),(1166913,1),(1148664,1),(1164040,1),(1023047,1)]
-tabela_destino = "FICHAFINANCEIRA"
-competencia_inicial = '2025-06-01'
-competencia_final = '2025-08-01'
-max_linhas_por_arquivo = 150
+matriculas = [(14630,1),(14671,1),(15942,1),(16057,1),(14200,1),(16594,1),(17104,1),(17112,1),(17196,1)]
+tabela_destino = "FichaFinanceiraCalculo"
+competencia_inicial = '2005-01-01'
+competencia_final = '2026-01-01'
+max_linhas_por_arquivo = None # maximo 150
+output_dir = "sql_inserts"
 
 conn = pyodbc.connect(
     f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
 )
 
-output_dir = "sql_inserts"
-os.makedirs(output_dir, exist_ok=True)
-
 for matricula,contrato in matriculas:
     try:
+        folderpath = os.path.join(output_dir, f"{matricula}_{contrato}")
+        
+        os.makedirs(folderpath, exist_ok=True)
+
         query = f"""
             SELECT * 
             FROM {tabela_destino}
@@ -57,16 +59,33 @@ for matricula,contrato in matriculas:
                     valores.append(str(v))
             valores_str = "(" + ", ".join(valores) + ")"
             valores_linhas.append(valores_str)
-            
-        total_blocos = math.ceil(len(valores_linhas) / max_linhas_por_arquivo)
         
+        if len(valores_linhas) == 0:
+            raise Exception(f"Nenhum valor encontrado na tabela {tabela_destino} para a matricula {matricula}-{contrato}") 
+            
+        if max_linhas_por_arquivo:
+            total_blocos = math.ceil(len(valores_linhas) / max_linhas_por_arquivo)
+        else:
+            total_blocos = 1 
+                   
         for i in range(total_blocos):
-            bloco = valores_linhas[i*max_linhas_por_arquivo:(i+1)*max_linhas_por_arquivo]
+            
+            if max_linhas_por_arquivo:
+                bloco = valores_linhas[i*max_linhas_por_arquivo:(i+1)*max_linhas_por_arquivo]
+            else:
+                bloco = valores_linhas
+                
             insert_sql = f"INSERT INTO {tabela_destino} ({colunas_str}) VALUES\n" + ",\n".join(bloco) + ";"
-            path = os.path.join(output_dir, f"insert_matricula_{matricula}_{contrato}_part{i+1}.sql")
+            
+            if total_blocos > 1:
+                file_name = f"insert_{tabela_destino}_part{i+1}.sql"
+            else:
+                file_name = f"insert_{tabela_destino}.sql"
+            
+            path = os.path.join(folderpath,file_name)
             with open(path, "w", encoding="utf-8") as f:
                 f.write(insert_sql)
 
-        print(f"Insert único gerado com {len(valores_linhas)} registros!")
+        print(f"Insert único gerado com {len(valores_linhas)} registros da matricula {matricula}-{contrato}!")
     except Exception as e:
         print(f"Ocorreu um erro ao gerar o insert da matricula {matricula}-{contrato}: {e}")
